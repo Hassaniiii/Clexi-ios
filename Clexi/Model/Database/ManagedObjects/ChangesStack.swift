534b4b2e61902ec78c462f8ed59f99cc2a387114
Hassaniiii <>
2018-01-15 17:24:56 +0330

    Project is re-structed

---
+++ Clexi/Model/Database/ManagedObjects/ChangesStack.swift
@@ -0,0 +1,22 @@
+//
+//  ChangesStack+CoreDataClass.swift
+//  Clexi
+//
+//  Created by Hassaniiii on 10/25/1396 AP.
+//  Copyright © 1396 AP Hassan Shahbazi. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+class ChangesStack: BLEClone {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ChangesStack> {
+        return NSFetchRequest<ChangesStack>(entityName: "ChangesStack")
+    }
+    
+    @NSManaged public var password: String?
+    @NSManaged public var changekey: Int16
+    @NSManaged public var hashkey: String?
+}
---
+++ Clexi/Model/Database/ManagedObjects/LocalAttributes.swift
@@ -0,0 +1,23 @@
+//
+//  LocalAttributes+CoreDataClass.swift
+//  Clexi
+//
+//  Created by Hassaniiii on 10/25/1396 AP.
+//  Copyright © 1396 AP Hassan Shahbazi. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+class LocalAttributes: NSManagedObject {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<LocalAttributes> {
+        return NSFetchRequest<LocalAttributes>(entityName: "LocalAttributes")
+    }
+    
+    @NSManaged public var id: Int16
+    @NSManaged public var popularity: Int32
+    @NSManaged public var lastused: NSDate?
+    @NSManaged public var local_ble: BLEClone?
+}
